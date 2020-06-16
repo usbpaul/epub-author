6a72b5ef2f07f0ac965c2e238fd0a6e650e7aa14
Paul Bakker <paul@vierdeweg.nl>
2020-06-16 17:56:25 +0200

    Generated by Atom.

---
+++ spec/epub-author-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import EpubAuthorView from '../lib/epub-author-view';
+
+describe('EpubAuthorView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
